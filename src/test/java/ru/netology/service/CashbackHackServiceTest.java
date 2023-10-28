ba179747c4a3c515734125564d80310763ab7779
zorinqamid-55 <zorinsegor@gmail.com>
2023-10-28 13:50:22 +0200

    first commit

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -12,7 +12,7 @@ public class CashbackHackServiceTest {
         int amount = 5700;
         int actual = service.remain(amount);
         int expected = 300;
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
     }
 
     @Test
@@ -20,22 +20,22 @@ public class CashbackHackServiceTest {
         int amount = 0;
         int actual = service.remain(amount);
         int expected = 1000;
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
     }
 
     @Test
     public void shouldCalculateIfAmount1000() {
         int amount = 1000;
         int actual = service.remain(amount);
-        int expected = 0;
-        assertEquals(expected, actual);
+        int expected = 1000;
+        assertEquals(actual, expected);
     }
 
     @Test
     public void shouldCalculateIfAmountIncorrect() {
         int amount = -50000;
         int actual = service.remain(amount);
-        int expected = 0;
-        assertEquals(expected, actual);
+        int expected = 1000;
+        assertEquals(actual, expected);
     }
 }
