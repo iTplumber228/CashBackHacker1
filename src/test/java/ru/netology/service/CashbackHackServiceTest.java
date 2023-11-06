4439b04645ac026b7484e5fd928b3a815c81dd61
Egor Zorin <121715161+iTplumber228@users.noreply.github.com>
2023-11-06 13:48:37 +0100

    Update CashbackHackServiceTest.java

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -5,10 +5,6 @@ import org.testng.annotations.Test;
 import static org.testng.Assert.*;
 
 public class CashbackHackServiceTest {
-
-public class CashbackHackServiceTest {
-    CashbackHackService service = new CashbackHackService();
-
     @Test
     public void shouldCalcCashBack() {
         CashbackHackService service = new CashbackHackService();
